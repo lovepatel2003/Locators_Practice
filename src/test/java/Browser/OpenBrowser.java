50411e2730207e3b2b0e6a605dd300f10c9d2ee4
lovepatel2003 <lloovveeppaatteell@gmail.com>
2020-06-23 13:15:44 -0400

    fianl
    fianl

--- src/test/java/Browser/OpenBrowser.java
+++ src/test/java/Browser/OpenBrowser.java
@@ -1,13 +1,18 @@
 package Browser;
 
+import org.junit.After;
+import org.junit.Before;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
 
 public class OpenBrowser {
     WebDriver driver;
     String browser="chrome";
 
+    @Before
     public WebDriver browserOpen(){
 
         if (browser.toLowerCase().equals("chrome")){
@@ -20,5 +25,12 @@ public class OpenBrowser {
         }
 
         return driver;
+    }
+
+    @After
+    public void tearDown(){
+        System.out.println("sbjdfkdsfjkdsfjds");
+        driver.quit();
+
     }
 }
